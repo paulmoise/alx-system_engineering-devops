ac8aee76e01bc7916334ba65e2653136f815c800
enigmatik100 <paul.gangbadja@erugis.fr>
2022-08-01 16:44:25 +0100

    Regex ruby

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
 # textme tasks
-puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
-
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
